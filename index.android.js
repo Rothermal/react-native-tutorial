a13e11dcaa7a51a51216d7c14bfc9cbf08579f87
Mark Rothermal <rothermal2@comcast.net>
2017-05-02 17:57:41 -0500

    flex

--- index.android.js
+++ index.android.js
@@ -10,13 +10,13 @@ import{ AppRegistry, View, Text } from'react-native';
 class FixedDimensionsBasics extends Component {
     render() {
         return(
-            <View>
-            <View style={{ width: 155, height: 70, backgroundColor:'powderblue'}}>
+            <View style={{flex:1}}>
+            <View style={{ flex:1, backgroundColor:'powderblue'}}>
             <Text>YAo</Text>
-            <View style={{width: 125, height: 25, backgroundColor:'red'}}/>
+            <View style={{flex:1, backgroundColor:'red'}}/>
             </View>
-            <View style={{ width: 100, height: 100, backgroundColor:'skyblue'}}/>
-            <View style={{ width: 150, height: 150, backgroundColor:'steelblue'}}/>
+            <View style={{ flex:2, backgroundColor:'skyblue'}}/>
+            <View style={{ flex:3, backgroundColor:'steelblue'}}/>
             </View>
         );
     }
